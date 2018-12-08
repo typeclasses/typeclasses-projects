9b45d0ebdad08c9a0b12a2e8b4b2533cdd961eb4
GinBaby <jdog74@gmail.com>
2018-12-08 00:14:01 -0500

    update clock-fltkhs

--- clock-fltkhs/app/Main.hs
+++ clock-fltkhs/app/Main.hs
@@ -6,28 +6,21 @@ import qualified Graphics.UI.FLTK.LowLevel.FL as FL
 import Graphics.UI.FLTK.LowLevel.Fl_Types
 import Graphics.UI.FLTK.Theme.Light
 
---buttonCb :: (?assets :: Assets) => Ref Button -> IO ()
---buttonCb b' = do
---  l' <- getLabel b'
---  if (l' == "Hello world")
---    then setLabel b' "Goodbye world"
---    else setLabel b' "Hello world"
 
 ui :: (?assets :: Assets) => IO ()
 ui = do
- window <- windowNew
+ window <- doubleWindowNew
            (Size (Width 300) (Height 300))
            Nothing
-           (Just "clock")
+           Nothing
  begin window
  clock <- clockNew
         (Rectangle (Position (X 50) (Y 50)) (Size (Width 200) (Height 200)))
         (Just "theme clock")
+ setLabelfont clock josefinSlabSemiBold
+ setLabelsize clock (FontSize 24)
  end window
  showWidget window
-
-centerPls :: Size -> Rectangle -> Rectangle
-centerPls w c = undefined
 
 main :: IO ()
 main = do
