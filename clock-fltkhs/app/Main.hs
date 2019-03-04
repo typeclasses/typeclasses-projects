d8aed2edc49eaf8c39d6b449407ba813a04dc5e0
GinBaby <jdog74@gmail.com>
2019-03-03 18:17:47 -0700

    updates to fltkhs clock

--- clock-fltkhs/app/Main.hs
+++ clock-fltkhs/app/Main.hs
@@ -5,6 +5,12 @@ module Main where
 import qualified Graphics.UI.FLTK.LowLevel.FL as FL
 import Graphics.UI.FLTK.LowLevel.Fl_Types
 import Graphics.UI.FLTK.Theme.Light
+import Graphics.UI.FLTK.LowLevel.Fl_Enumerations
+
+--buttonCb :: (?assets :: Assets) => Ref Button -> IO ()
+--buttonCb button = do
+--  putStrLn "hey"
+--  undefined
 
 
 ui :: (?assets :: Assets) => IO ()
@@ -15,10 +21,15 @@ ui = do
            Nothing
  begin window
  clock <- clockNew
-        (Rectangle (Position (X 50) (Y 50)) (Size (Width 200) (Height 200)))
+        (Rectangle (Position (X 50) (Y 50))
+        (Size (Width 200) (Height 200)))
         (Just "theme clock")
  setLabelfont clock josefinSlabSemiBold
  setLabelsize clock (FontSize 24)
+ setResizable window (Just clock)
+ sizeRangeWithArgs window (Size (Width 200) (Height 200))
+   (OptionalSizeRangeArgs (Just 400) (Just 400)
+       Nothing Nothing (Just True))
  end window
  showWidget window
 
